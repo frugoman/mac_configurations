095c01c0d16ac05151317b98395875bc4e276e6d
Nicolas Frugoni <nfrugoni19@gmail.com>
2020-10-01 16:54:49 -0300

    Create setup-script.sh

---
+++ setup-script.sh
@@ -0,0 +1,7 @@
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew update
+brew cask install iterm2
+// Profiles - Keys - Presets -> Natural Text Editing
+// Genera -> Reuse previous session directory
+brew install bash
