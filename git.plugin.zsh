0ea7a21d7eb99449eb5212b2d0f08148a666f944
Nicolas Frugoni <nfrugoni19@gmail.com>
2022-04-25 14:37:41 +0100

    Update git.plugin.zsh

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -2,10 +2,6 @@
 autoload -Uz is-at-least
 git_version="${${(As: :)$(git version 2>/dev/null)}[3]}"
 
-#
-# Functions
-#
-
 # Pretty log messages
 function _git_log_prettily(){
   if ! [ -z $1 ]; then
@@ -30,6 +26,6 @@ alias gcm="git commit -m"
 alias gpo='git push -u origin "$(git_current_branch)"'
 alias gd="git diff"
 alias gco="git checkout"
-alias gb="git branch"
+alias gb="git checkout -b"
 
 unset git_version
